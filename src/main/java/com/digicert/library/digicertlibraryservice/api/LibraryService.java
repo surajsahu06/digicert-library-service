cdd1a07594a040ace2cd9769d0bbeb453ade8107
Sooraj Sahoo <ssahoo@overstock.com>
2020-12-14 00:35:43 -0700

    updated the java docs for api

--- src/main/java/com/digicert/library/digicertlibraryservice/api/LibraryService.java
+++ src/main/java/com/digicert/library/digicertlibraryservice/api/LibraryService.java
@@ -20,15 +20,28 @@ public interface LibraryService {
   Optional<BookDetailEntity> findBookById(Integer bookId);
 
   /**
-   * This method will update the qty of book if the entry is already present in database table, else create the new
-   * entry for the book
+   * This method will create the entry in database table, if the bookId already present, it will just response with
+   * existing entry in the book detail table
    *
    * @param bookDetailAddRequest
    * @return
    */
   BookDetailEntity createBookDetailEntry(BookDetailAddRequest bookDetailAddRequest);
 
+  /**
+   * Delete the book detail entry from table if present
+   * @param bookId
+   * @throws BookNotFoundException
+   */
   void deleteBookDetailEntry(Integer bookId) throws BookNotFoundException;
+
+  /**
+   * Update the existing entry in table by bookId
+   * @param bookId
+   * @param bookDetailUpdateRequest
+   * @return
+   * @throws BookNotFoundException
+   */
 
   BookDetailEntity updateBookDetailEntry(Integer bookId, BookDetailUpdateRequest bookDetailUpdateRequest)
     throws BookNotFoundException;
